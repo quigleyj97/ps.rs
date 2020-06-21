b6213aa5f5acb229b646414afeeef20fead3d2e9
Joe Quigley <quigley.joseph@outlook.com>
2020-06-20 18:28:22 -0700

    Implement J, OR

--- src/devices/cpu.rs
+++ src/devices/cpu.rs
@@ -34,6 +34,12 @@ pub struct CpuState {
     /// which represent how many cycles the CPU will be blocked executing that
     /// read.
     pub wait: u32,
+    /// The next instruction to execute in the pipeline, as a word
+    ///
+    /// This is implemented to simulate delay slots, which occur due to how the
+    /// MIPS architecture handles (or more accurately, doesn't handle) branch
+    /// hazards in instructions.
+    next_instruction: u32,
 }
 
 pub const CPU_POWERON_STATE: CpuState = CpuState {
@@ -41,6 +47,7 @@ pub const CPU_POWERON_STATE: CpuState = CpuState {
     pc: 0xBFC0_0000,
     // the rest of this is shooting from the hip
     registers: [0u32; 32],
+    next_instruction: 0x0000_00000,
     wait: 0,
 };
 
@@ -89,8 +96,8 @@ pub fn tick<T: WithCpu>(mb: &mut T) -> bool {
 /// Unconditionally advance the state of the CPU
 pub fn exec<T: WithCpu + BusDevice>(mb: &mut T) {
     let pc = mb.cpu().state.pc;
-    let word = mb.read32(pc);
-    let (mnemonic, instruction) = decode_instruction(word);
+    let (mnemonic, instruction) = decode_instruction(mb.cpu().state.next_instruction);
+    mb.cpu_mut().state.next_instruction = mb.read32(pc);
     println!("STEP {:?} 0x{:08X}", mnemonic, *instruction);
     let fn_handler = match_handler::<T>(mnemonic);
     fn_handler(mb, instruction);
@@ -106,17 +113,74 @@ pub fn exec<T: WithCpu + BusDevice>(mb: &mut T) {
 #[allow(type_alias_bounds)] // leaving this in for self-documenting reasons
 type OpcodeHandler<T: WithCpu + BusDevice> = fn(&mut T, Instruction);
 
+#[rustfmt::skip]
 fn match_handler<T: WithCpu + BusDevice>(mnemonic: Mnemonic) -> OpcodeHandler<T> {
     match mnemonic {
         Mnemonic::ADD => op_add,
         Mnemonic::ADDI => op_addi,
         Mnemonic::ADDIU => op_addiu,
         Mnemonic::ADDU => op_addu,
+        Mnemonic::AND =>            /*op_and,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::ANDI =>           /*op_andi,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BEQ =>            /*op_beq,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BGEZ =>           /*op_bgez,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BGEZAL =>         /*op_bgezal,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BGTZ =>           /*op_bgtz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BLEZ =>           /*op_blez,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BLTZ =>           /*op_bltz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BLTZAL =>         /*op_bltzal,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BNE =>            /*op_bne,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::BREAK =>          /*op_break,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::CFCz =>           /*op_cfcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::COPz =>           /*op_copz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::CTCz =>           /*op_ctcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::DIV =>            /*op_div,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::DIVU =>           /*op_divu,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::J => op_j,
+        Mnemonic::JAL =>            /*op_jal,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::JALR =>           /*op_jalr,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::JR =>             /*op_jr,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LB =>             /*op_lb,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LBU =>            /*op_lbu,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LH =>             /*op_lh,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LHU =>            /*op_lhu,*/todo!("instr {:?}", mnemonic),
         Mnemonic::LUI => op_lui,
+        Mnemonic::LW =>             /*op_lw,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LWCz =>           /*op_lwcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LWL =>            /*op_lwl,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::LWR =>            /*op_lwr,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MFCz =>           /*op_mfcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MFHI =>           /*op_mfhi,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MFLO =>           /*op_mflo,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MTCz =>           /*op_mtcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MTHI =>           /*op_mthi,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MTLO =>           /*op_mtlo,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MULT =>           /*op_mult,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::MULTU =>          /*op_multu,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::NOR =>            /*op_nor,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::OR => op_or,
         Mnemonic::ORI => op_ori,
+        Mnemonic::SB =>             /*op_sb,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SH =>             /*op_sh,*/todo!("instr {:?}", mnemonic),
         Mnemonic::SLL => op_sll,
+        Mnemonic::SLLV =>           /*op_sllv,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SLT =>            /*op_slt,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SLTI =>           /*op_slti,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SLTIU =>          /*op_sltiu,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SLTU =>           /*op_sltu,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SRA =>            /*op_sra,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SRAV =>           /*op_srav,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SRL =>            /*op_srl,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SRLV =>           /*op_srlv,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SUB =>            /*op_sub,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SUBU =>           /*op_subu,*/todo!("instr {:?}", mnemonic),
         Mnemonic::SW => op_sw,
-        _ => panic!("Operation {:?} not implemented", mnemonic),
+        Mnemonic::SWCz =>           /*op_swcz,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SWL =>            /*op_swl,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SWR =>            /*op_swr,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::SYSCALL =>        /*op_syscall,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::XOR =>            /*op_xor,*/todo!("instr {:?}", mnemonic),
+        Mnemonic::XORI =>           /*op_xori,*/todo!("instr {:?}", mnemonic),
     }
 }
 
@@ -164,6 +228,15 @@ op_fn!(op_addu, (mb, instr), {
 
 // skip
 
+op_fn!(op_j, (mb, instr), {
+    let target = instr.target() << 2;
+    let new_pc = target | mb.cpu().state.pc & 0xF000_0000; // select the 4 MSBs of the old PC
+    mb.cpu_mut().state.pc = new_pc - 4; // correct for the PC advance later
+    mb.cpu_mut().state.wait += 1; //
+});
+
+// skip
+
 op_fn!(op_lui, (mb, instr), {
     let data = u32::from(instr.immediate()) << 16;
     let cpu = mb.cpu_mut();
@@ -171,6 +244,14 @@ op_fn!(op_lui, (mb, instr), {
 });
 
 // skip
+
+op_fn!(op_or, (mb, instr), {
+    let source = instr.rs() as usize;
+    let target = instr.rt() as usize;
+    let dest = instr.rd() as usize;
+    let cpu = mb.cpu_mut();
+    write_reg(cpu, dest, get_reg(cpu, source) | get_reg(cpu, target));
+});
 
 op_fn!(op_ori, (mb, instr), {
     let source = instr.rs() as usize;
